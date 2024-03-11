bf90df210f64a72a5ffeb77b15031eb1cc353988
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-03-11 17:14:48 +0100

    some

---
+++ stegamelioree.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from customtkinter import (CTkButton as Button,
+                           CTk as Tk, 
+                           CTkEntry as Entry,
+                           CTkFrame as Frame,
+                           CTkTextbox as Text,
+                           CTkLabel as Label,
+                           CTkCheckBox as Checkbutton)
+# todo: copy old gui and add the possibility to insert json as editable table 
